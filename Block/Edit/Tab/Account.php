99654e4924c133cd96381ef729e23b93bd28bd69
vinai <vinai@ac01b695-cdaf-4f09-91c8-82da1b8b2c39>
2008-12-30 17:02:37 +0000

    - Fixed create new customer form in adminhtml (thanks to SeL for the bug report). - Upped the version

--- Block/Edit/Tab/Account.php
+++ Block/Edit/Tab/Account.php
@@ -39,30 +39,33 @@ class Netzarbeiter_CustomerActivation_Block_Edit_Tab_Account extends Mage_Adminh
 		parent::initForm();
 
         $customer = Mage::registry('current_customer');
-		if ($customer->getId()) {
-			$activationAttribute = $customer->getAttribute('customer_activated');
+        
+        // do not check if customer is activated, so the create new customer
+        // form in the adminhtml works (Once again thanks to SeL for reporting the bug!)
+        
+		$activationAttribute = $customer->getAttribute('customer_activated');
 
-			$fieldset = $this->getForm()->getElement('base_fieldset'); // fieldset form element
+		$fieldset = $this->getForm()->getElement('base_fieldset'); // fieldset form element
 
-			// remove default text field element
-			$fieldset->removeField('customer_activated');
+		// remove default text field element
+		$fieldset->removeField('customer_activated');
 			
-			/**
-			 * Thanks to SeL for this bugfix!
-			 */
-			if (Mage::getStoreConfig('customer/customeractivation/disable_ext', $customer->getStoreId())) return $this;
+		/**
+		 * Thanks to SeL for this bugfix!
+		 */
+		if (Mage::getStoreConfig('customer/customeractivation/disable_ext', $customer->getStoreId())) return $this;
 
-			// add new select element
-			$element = $fieldset->addField('customer_activated', 'select', array(
-				'name' => 'customer_activated',
-				'label' => Mage::helper('customer')->__($activationAttribute->getFrontendLabel()),
-			));
-			$element->setEntityAttribute($activationAttribute)->setValues(array(
-				0 => Mage::helper('customer')->__('Deactivated'),
-				1 => Mage::helper('customer')->__('Activated')
-			));
-			$element->setValue((int) $customer->getData('customer_activated'));
-		}
+		// add new select element
+		$element = $fieldset->addField('customer_activated', 'select', array(
+			'name' => 'customer_activated',
+			'label' => Mage::helper('customer')->__($activationAttribute->getFrontendLabel()),
+		));
+		$element->setEntityAttribute($activationAttribute)->setValues(array(
+			0 => Mage::helper('customer')->__('Deactivated'),
+			1 => Mage::helper('customer')->__('Activated')
+		));
+		$element->setValue((int) $customer->getData('customer_activated'));
+		
 		return $this;
 	}
 }
