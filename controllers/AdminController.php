a89f96d045ae5bed9da6dc8805c695feee1a78b1
vinai <vinai@netzarbeiter.com>
2010-11-01 22:05:56 +0100

    Add reset() call on customer model before loading. Also, fix formating.

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -4,35 +4,36 @@ class Netzarbeiter_CustomerActivation_AdminController extends Mage_Adminhtml_Con
 {
 	public function massActivationAction()
 	{
-		$customerIds = $this -> getRequest() -> getParam('customer');
+		$customerIds = $this->getRequest()->getParam('customer');
 
-		if(!is_array($customerIds))
+		if(! is_array($customerIds))
 		{
-			Mage::getSingleton('adminhtml/session') -> addError(Mage::helper('adminhtml') -> __('Please select item(s)'));
+			Mage::getSingleton('adminhtml/session')->addError(Mage::helper('adminhtml')->__('Please select item(s)'));
 		}
 		else
 		{
 			$model = Mage::getModel('customer/customer');
+			/* @var $model Mage_Customer_Model_Customer */
 			try
 			{
 				foreach($customerIds as $customerId)
 				{
-					$model	-> load($customerId);
-					$model -> setCustomerActivated($this -> getRequest() -> getParam('customer_activated')) -> save();
+					$model->reset()->load($customerId);
+					$model->setCustomerActivated($this->getRequest()->getParam('customer_activated'))->save();
 				}
 
-				Mage::getSingleton('adminhtml/session') -> addSuccess(
-					Mage::helper('adminhtml') -> __(
+				Mage::getSingleton('adminhtml/session')->addSuccess(
+					Mage::helper('adminhtml')->__(
 						'Total of %d record(s) were successfully saved', count($customerIds)
 					)
 				);
 			}
 			catch(Exception $e)
 			{
-				Mage::getSingleton('adminhtml/session') -> addError($e -> getMessage());
+				Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
 			}
 		}
 
-		$this -> _redirect('adminhtml/customer');
+		$this->_redirect('adminhtml/customer');
 	}
 }
