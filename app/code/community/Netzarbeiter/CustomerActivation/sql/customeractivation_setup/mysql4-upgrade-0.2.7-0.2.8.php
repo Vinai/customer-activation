992f8db26e4f91a3a791894445e2d16fe0b366a5
Vinai Kopp <vinai@netzarbeiter.com>
2011-03-23 17:48:14 +0100

    Push version to 0.3.0 and add upgrade script to add customer attribute to be dispayed in the admin form

---
+++ app/code/community/Netzarbeiter/CustomerActivation/sql/customeractivation_setup/mysql4-upgrade-0.2.7-0.2.8.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * package    Netzarbeiter_CustomerActivation
+ * copyright  Copyright (c) 2011 Vinai Kopp http://netzarbeiter.com/
+ * license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/* @var $installer Mage_Customer_Model_Entity_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+/*
+ * Add required information for Magento 1.4.2
+ */
+if (version_compare(Mage::getVersion(), '1.4.2', '>='))
+{
+	Mage::getSingleton('eav/config')
+		->getAttribute('customer', 'customer_activated')
+		->setData('used_in_forms', array('adminhtml_customer'))
+		->save();
+}
+
+
+$installer->endSetup();
