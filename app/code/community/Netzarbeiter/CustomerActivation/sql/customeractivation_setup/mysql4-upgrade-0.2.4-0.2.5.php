abdf08f584293d40bc14fce0e64bfba48f509a16
Vinai Kopp <vinai@netzarbeiter.com>
2011-07-13 08:32:44 +0200

    Add variable type hint

--- app/code/community/Netzarbeiter/CustomerActivation/sql/customeractivation_setup/mysql4-upgrade-0.2.4-0.2.5.php
+++ app/code/community/Netzarbeiter/CustomerActivation/sql/customeractivation_setup/mysql4-upgrade-0.2.4-0.2.5.php
@@ -17,6 +17,7 @@
  * license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+/* @var $this Mage_Customer_Model_Entity_Setup */
 $this->startSetup();
 
 $customer = Mage::getModel('customer/customer');
