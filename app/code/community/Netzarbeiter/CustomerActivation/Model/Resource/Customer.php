f3c4f8d53bb3f130340a514a7018c76583d45632
Vinai Kopp <vinai@netzarbeiter.com>
2013-05-24 22:14:59 +0200

    Add license header comment

--- app/code/community/Netzarbeiter/CustomerActivation/Model/Resource/Customer.php
+++ app/code/community/Netzarbeiter/CustomerActivation/Model/Resource/Customer.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * package    Netzarbeiter_CustomerActivation
+ * copyright  Copyright (c) 2013 Vinai Kopp http://netzarbeiter.com/
+ * license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 class Netzarbeiter_CustomerActivation_Model_Resource_Customer
     extends Mage_Eav_Model_Entity_Abstract
