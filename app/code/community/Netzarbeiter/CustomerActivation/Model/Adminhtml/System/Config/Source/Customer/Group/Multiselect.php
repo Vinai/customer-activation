25840bb4241784635e162df02fa00a28940b3d04
Vinai Kopp <vinai@netzarbeiter.com>
2013-05-24 22:15:35 +0200

    Add custom config source model for customer group multiselect
    Unable to use adminhtml/system_config_source_customer_group_multiselect because
    that is only available since Magento 1.7/1.12.

---
+++ app/code/community/Netzarbeiter/CustomerActivation/Model/Adminhtml/System/Config/Source/Customer/Group/Multiselect.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * package    Netzarbeiter_CustomerActivation
+ * copyright  Copyright (c) 2013 Vinai Kopp http://netzarbeiter.com/
+ * license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Class Netzarbeiter_CustomerActivation_Model_Adminhtml_System_Config_Source_Customer_Group_Multiselect
+ *
+ * Unable to use config source class provided in the core since that is only available since
+ * Magento 1.7/1.12, this module supports backward compatibility to 1.4.
+ *
+ * @see Mage_Adminhtml_Model_System_Config_Source_Customer_Group_Multiselect
+ */
+class Netzarbeiter_CustomerActivation_Model_Adminhtml_System_Config_Source_Customer_Group_Multiselect
+{
+    /**
+     * Customer groups options array
+     *
+     * @var null|array
+     */
+    protected $_options;
+
+    /**
+     * Retrieve customer groups as array
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        if (!$this->_options) {
+            $this->_options = Mage::getResourceModel('customer/group_collection')
+                ->setRealGroupsFilter()
+                ->loadData()->toOptionArray();
+        }
+        return $this->_options;
+    }
+}
