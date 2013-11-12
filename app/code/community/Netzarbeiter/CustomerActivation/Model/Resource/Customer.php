c69ddb823b7c74013976b4c1d092acd5c6c9e64a
Vinai Kopp <vinai@netzarbeiter.com>
2013-11-12 19:53:44 +0100

    Fix: de-association of selected default addresses on mass updates

--- app/code/community/Netzarbeiter/CustomerActivation/Model/Resource/Customer.php
+++ app/code/community/Netzarbeiter/CustomerActivation/Model/Resource/Customer.php
@@ -43,16 +43,27 @@ class Netzarbeiter_CustomerActivation_Model_Resource_Customer
         if ($customerIds) {
             $attribute = $this->getAttribute('customer_activated');
             $table = $attribute->getBackend()->getTable();
-            $select = $this->_getReadAdapter()->select()
+            
+            $select = $this->getReadConnection()->select()
                 ->from($table, 'entity_id')
                 ->where('entity_id IN (?)', $customerIds)
+                ->where('attribute_id = ?', $attribute->getId())
+                ->where('value = ?', $value);
+            $noChangeIds = $this->_getReadAdapter()->fetchCol($select);
+            
+            $changeIds = array_diff($customerIds, $noChangeIds);
+            $select = $this->_getReadAdapter()->select()
+                ->from($table, 'entity_id')
+                ->where('entity_id IN (?)', $changeIds)
                 ->where('attribute_id = ?', $attribute->getId());
 
             $updateIds = $this->_getReadAdapter()->fetchCol($select);
-            $insertIds = array_diff($customerIds, $updateIds);
+            $insertIds = array_diff($changeIds, $updateIds);
 
             if ($updateIds) {
-                $cond = $this->_getWriteAdapter()->quoteInto('entity_id IN (?)', $updateIds);
+                $cond = $this->_getWriteAdapter()->quoteInto('entity_type_id = ?', $this->getEntityType()->getId());
+                $cond .= $this->_getWriteAdapter()->quoteInto(' AND attribute_id = ?', $attribute->getId());
+                $cond .= $this->_getWriteAdapter()->quoteInto(' AND entity_id IN (?)', $updateIds);
                 $this->_getWriteAdapter()->update($table, array('value' => $value), $cond);
             }
             if ($insertIds) {
