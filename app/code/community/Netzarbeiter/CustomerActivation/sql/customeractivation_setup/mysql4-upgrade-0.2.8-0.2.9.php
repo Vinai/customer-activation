c122a120828939924446c27d7abf49c688aeb6dd
Vinai Kopp <vinai@netzarbeiter.com>
2014-08-24 08:09:59 +1000

    Activate all existing customers by default.
    This setup script will only execute for new installs, as I haven't incremented the version.
    Closes #53.

---
+++ app/code/community/Netzarbeiter/CustomerActivation/sql/customeractivation_setup/mysql4-upgrade-0.2.8-0.2.9.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * package    Netzarbeiter_CustomerActivation
+ * copyright  Copyright (c) 2014 Vinai Kopp http://netzarbeiter.com/
+ * license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/* @var $installer Mage_Customer_Model_Entity_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$resource = Mage::getResourceModel('customer/customer');
+
+// Set all existing customers to true
+$select = $installer->getConnection()->select()
+    ->from($resource->getEntityTable(), $resource->getEntityIdField());
+$customerIds = $installer->getConnection()->fetchCol($select);
+
+$updatedCustomerIds = Mage::getResourceModel('customeractivation/customer')
+    ->massSetActivationStatus($customerIds, 1);
+
+$installer->endSetup();
